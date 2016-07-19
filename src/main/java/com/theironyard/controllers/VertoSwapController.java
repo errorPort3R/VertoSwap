45f8d4ae05bbb947afd5c5ffdc55a4c558c3d463
jeffryporter <jporter7@gmail.com>
2016-07-19 16:14:29 -0400

    Add Thread routes.

--- src/main/java/com/theironyard/controllers/VertoSwapController.java
+++ src/main/java/com/theironyard/controllers/VertoSwapController.java
@@ -3,6 +3,7 @@ package com.theironyard.controllers;
 import com.theironyard.entities.Item;
 import com.theironyard.entities.User;
 import com.theironyard.entities.Work;
+import com.theironyard.entities.Thread;
 import com.theironyard.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -281,4 +282,253 @@ public class VertoSwapController
         return "redirect:/";
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @RequestMapping(path = "/thread-create", method = RequestMethod.POST)
+    public String createThread(HttpSession session, User receiver, Item item)
+    {
+        String username = (String)session.getAttribute("username");
+        User user = users.findByName(username);
+        Thread t = new Thread(user,receiver, item);
+        threads.save(t);
+        session.setAttribute("username", user.getUsername());
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/thread-read", method = RequestMethod.GET)
+    public String getThread(HttpSession session)
+    {
+        String username = (String)session.getAttribute("username");
+        User user = users.findByName(username);
+        Iterable<Item> itemsList = items.findByUser(user);
+        session.setAttribute("username", user.getUsername());
+        return"";
+    }
+
+    @RequestMapping(path = "/thread-update", method = RequestMethod.POST)
+    public String updateThread(HttpSession session,int id, User receiver, Item item)
+    {
+        String username = (String)session.getAttribute("username");
+        User user = users.findByName(username);
+        Thread t = new Thread(user,receiver, item);
+        t.setId(id);
+        threads.save(t);
+        session.setAttribute("username", user.getUsername());
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/thread-delete", method = RequestMethod.POST)
+    public String deleteThread(HttpSession session, int id)
+    {
+        String username = (String)session.getAttribute("username");
+        User user = users.findByName(username);
+        works.delete(id);
+        session.setAttribute("username", user.getUsername());
+        return "redirect:/";
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    @RequestMapping(path = "/photo-create", method = RequestMethod.POST)
+    public String createThread(HttpSession session, User receiver, Item item)
+    {
+        String username = (String)session.getAttribute("username");
+        User user = users.findByName(username);
+        Thread t = new Thread(user,receiver, item);
+        threads.save(t);
+        session.setAttribute("username", user.getUsername());
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/thread-read", method = RequestMethod.GET)
+    public String getThread(HttpSession session)
+    {
+        String username = (String)session.getAttribute("username");
+        User user = users.findByName(username);
+        Iterable<Item> itemsList = items.findByUser(user);
+        session.setAttribute("username", user.getUsername());
+        return"";
+    }
+
+    @RequestMapping(path = "/thread-update", method = RequestMethod.POST)
+    public String updateThread(HttpSession session,int id, User receiver, Item item)
+    {
+        String username = (String)session.getAttribute("username");
+        User user = users.findByName(username);
+        Thread t = new Thread(user,receiver, item);
+        t.setId(id);
+        threads.save(t);
+        session.setAttribute("username", user.getUsername());
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/thread-delete", method = RequestMethod.POST)
+    public String deleteThread(HttpSession session, int id)
+    {
+        String username = (String)session.getAttribute("username");
+        User user = users.findByName(username);
+        works.delete(id);
+        session.setAttribute("username", user.getUsername());
+        return "redirect:/";
+
+    }
+
 }
