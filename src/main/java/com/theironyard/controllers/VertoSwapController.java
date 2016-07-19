16fd41602effad59961e4004ba9948a5be2364a2
Dan <danielandrewvoss@gmail.com>
2016-07-19 15:19:19 -0400

    Create login, logout, create account routes

--- src/main/java/com/theironyard/controllers/VertoSwapController.java
+++ src/main/java/com/theironyard/controllers/VertoSwapController.java
@@ -2,25 +2,66 @@ package com.theironyard.controllers;
 
 import com.theironyard.entities.User;
 import com.theironyard.services.UserRepository;
+import com.theironyard.utilities.PasswordStorage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 
 /**
  * Created by Dan on 7/19/16.
  */
-@RestController
+@Controller
 public class VertoSwapController {
 
     @Autowired
     UserRepository users;
 
+
+
+
+
+
+
+
+
+
+
+
+
+    @RequestMapping(path = "/account-create", method = RequestMethod.POST)
+    public String createAccount(HttpSession session, String username, String password) throws Exception {
+        User user = users.findByName(username);
+        if (user != null) {
+            throw new Exception("Username unavailable.");
+        }
+        else {
+            user = new User(username, PasswordStorage.createHash(password));
+            users.save(user);
+            session.setAttribute("username", username);
+        }
+        return "redirect:/";
+    }
+
     @RequestMapping(path = "/login", method = RequestMethod.POST)
-    public void login(HttpSession session, @RequestBody User user) {
-        //User userFromDB = users.findByName
+    public String login(HttpSession session, String username, String password) throws Exception {
+        User userFromDB = users.findByName(username);
+        if (userFromDB == null) {
+            return "redirect:/create-account";
+        }
+        else if (!PasswordStorage.verifyPassword(password, userFromDB.getPassword())) {
+            throw new Exception("Wrong password.");
+        }
+        session.setAttribute("username", username);
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/logout", method = RequestMethod.POST)
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/";
     }
 }
