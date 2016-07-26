ec4db7aee78c560c84473609963352a37a1f854d
Dan <danielandrewvoss@gmail.com>
2016-07-26 13:06:59 -0400

    Add search

--- src/main/java/com/theironyard/services/ItemRepository.java
+++ src/main/java/com/theironyard/services/ItemRepository.java
@@ -2,16 +2,25 @@ package com.theironyard.services;
 
 import com.theironyard.entities.Item;
 import com.theironyard.entities.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by Dan on 7/19/16.
  */
-public interface ItemRepository extends CrudRepository<Item, Integer>
-{
+public interface ItemRepository extends CrudRepository<Item, Integer> {
     public Iterable<Item> findByUser(User user);
+
     public Iterable<Item> findByServiceTrueOrderByTimeDesc();
+
     public Iterable<Item> findByServiceFalseOrderByTimeDesc();
+
     public Iterable<Item> findByUserAndStatus(User user, Item.Status status);
+
     public Iterable<Item> findByUserAndStatusOrderByTimeDesc(User user, Item.Status status);
+
+    public Iterable<Item> findByTitleLikeOrLocationLikeOrDescriptionLikeOrAcceptableExchangeLike(String title, String location, String description, String acceptableExchange);
+
+
+
 }
