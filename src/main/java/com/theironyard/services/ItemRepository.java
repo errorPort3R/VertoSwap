fda31f85e992bef62b1f830492b3bd7d87ff8e21
jeffryporter <jporter7@gmail.com>
2016-08-02 15:47:46 -0400

    Repair work delete when attached to an item.

--- src/main/java/com/theironyard/services/ItemRepository.java
+++ src/main/java/com/theironyard/services/ItemRepository.java
@@ -2,6 +2,7 @@ package com.theironyard.services;
 
 import com.theironyard.entities.Item;
 import com.theironyard.entities.User;
+import com.theironyard.entities.Work;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -21,7 +22,7 @@ public interface ItemRepository extends CrudRepository<Item, Integer> {
 
     public Iterable<Item> findByTitleLikeOrLocationLikeOrDescriptionLikeOrAcceptableExchangeLike(String title, String location, String description, String acceptableExchange);
 
-
+    public Iterable<Item> findByWork(Work work);
     //
 
     @Query("SELECT i from Item i WHERE LOWER(title) LIKE '%' || LOWER(?) || '%' OR LOWER(location) LIKE '%' || LOWER(?) || '%' OR LOWER(description) LIKE '%' || LOWER(?) || '%' OR LOWER(acceptableExchange) LIKE '%' || LOWER(?) || '%'")
